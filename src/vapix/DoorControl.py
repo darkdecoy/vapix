551b02ed8129772ad8a31a095806ae2398d27033
Dark Decoy <darkdecoy@notracking.space>
2025-08-20 23:39:09 +0000

    refactor: updated doorcontrol object to create door objects for all doors on the door controller

--- src/vapix/DoorControl.py
+++ src/vapix/DoorControl.py
@@ -10,7 +10,37 @@ if TYPE_CHECKING:
 class DoorControl:
 
     def __init__(self, api: VapixAPI) -> None:
-        
+
+        self.api = api
+        self.api.base_url = "http://" + self.api.host + "/vapix"
+
+        self.doors = {}
+
+        self.get_doors()
+
+    def get_doors(self):
+        """
+        Gets the door info.
+
+        Returns:
+            list: A dictionary containing door info.
+        """
+        resp = self.api._send_request(
+            "doorcontrol/GetDoorInfoList",
+        )
+
+        data = json.loads(resp)
+
+        for door in data['DoorInfo']:
+
+            self.doors[door['Name']] = Door(token=door['token'], controller=self)
+
+class Door:
+
+    def __init__(self, token, controller: DoorControl) -> None:
+
+        self.controller = controller
+
         self.actions_table = {
             "Blocked": ("Block","DoubleLock", "Lock", "LockDown", "LockDownRelease", "LockOpen", "LockOpenRelease", "Unlock",),
             "DoubleLocked": ("Block","DoubleLock","Lock","LockDown","LockDownRelease","LockOpen","LockOpenRelease","Unlock"),
@@ -24,6 +54,21 @@ class DoorControl:
 
         self.actions = ("Block","DoubleLock","Lock","LockDown","LockDownRelease","LockOpen","LockOpenRelease","Unlock")
 
+        self.Name = ''
+        self.Description = ''
+        self.Capabilities = {}
+        self.token = token
+        
+        self.DoorPhysicalState = 'Closed'
+        self.Alarm = 'Normal'
+        self.Mode = 'Unlocked'
+
+        self.LastUpdate = True
+        self.Status = 'Door Created'
+        
+        self.update_info()
+        self.update_status()
+
     def _check_action(self, token, action) -> dict:
         """
         Gets the door info.
@@ -31,28 +76,11 @@ class DoorControl:
         Returns:
             list: A dictionary containing door state info.
         """
-        state = self.get_state(token=token)
-        info = self.get_info(token=token)
 
-        if action in self.actions_table[state['DoorMode']] and info[0]["Capabilities"][action]:
+        if action in self.actions_table[self.Mode] and self.Capabilities[action]:
             return True
         else:
             return False
-
-class Door(DoorControl):
-
-    def __init__(self, api: VapixAPI, token) -> None:
-        self.api = api
-        self.api.base_url = "http://" + self.api.host + "/vapix"
-
-        self.token = token
-
-        self.DoorPhysicalState = 'Closed'
-        self.Alarm = 'Normal'
-        self.Mode = 'Unlocked'
-        self.LastUpdate = True
-        self.Status = 'State Updated'
-        self.info = {}
     
     def update_info(self) -> dict:
         """
@@ -61,50 +89,51 @@ class Door(DoorControl):
         Returns:
             list: A dictionary containing door info.
         """
-        resp = self.api._send_request(
+        resp = self.controller.api._send_request(
             "doorcontrol/GetDoorInfo",
             params={"Token": self.token},
         )
 
         data = json.loads(resp)
 
-        self.info = data
+        self.Name = data['DoorInfo'][0]['Name']
+        self.Description = data['DoorInfo'][0]['Description']
+        self.Capabilities = data['DoorInfo'][0]['Capabilities']
+        self.token = data['DoorInfo'][0]['token']
 
-    def update_state(self) -> dict:
+    def update_status(self) -> dict:
         """
         Gets the door info.
 
         Returns:
             list: A dictionary containing door state info.
         """
-        resp = self.api._send_request(
+        resp = self.controller.api._send_request(
             "doorcontrol/GetDoorState",
             params={"Token": self.token},
         )
 
         data = json.loads(resp)
 
-        self.DoorPhysicalState = data.DoorPhysicalState
-        self.Alarm = data.Alarm
-        self.Mode = data.DoorMode
+        self.DoorPhysicalState = data['DoorState']['DoorPhysicalState']
+        self.Alarm = data['DoorState']['Alarm']
+        self.Mode = data['DoorState']['DoorMode']
 
-    def _update_mode(self, action) -> dict:
+    def update_mode(self, action) -> dict:
         """
         Update Door.
         """
 
-        if action in actions and self._check_action(token=self.token, action=action):
+        if action in self.actions and self._check_action(token=self.token, action=action):
 
-            state = self.update_state(self)
-
-            resp = self.api._send_request(
+            resp = self.controller.api._send_request(
                 ("doorcontrol/" + action + "Door"),
                 params={"Token": self.token},
             )
 
-            state = self.update_state(self)
+            self.update_status()
 
-            if state['DoorMode'].startswith(action):
+            if self.Mode.startswith(action):
                 self.LastUpdate = True
                 self.Status = "State Updated"
             else:
@@ -112,7 +141,6 @@ class Door(DoorControl):
                 self.Status = "State Not Updated"
 
         else:
-            state = self.get_state(token=token)
 
             self.LastUpdate = False
             self.Status = "Action Not Supported"
