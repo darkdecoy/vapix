dbaa7c5fd37cc810d05d94f78e4d9f98b00130a2
Dark Decoy <darkdecoy@notracking.space>
2025-09-02 23:45:39 +0000

    refactor: updated test script

--- src/test.py
+++ src/test.py
@@ -5,6 +5,7 @@ control1 = api(host="192.168.1.137", user="root", password="changem3")
 
 doors = control1.doorcontrol.doors
 schedules = control1.schedule.schedules
+current = doors['Door 1'].get_schedule()
 
 schedule_name = 'Test'
 
@@ -12,6 +13,6 @@ original = schedules[schedule_name].calendar
 
 print(original)
 
-doors['Door 1'].set_schedule(ScheduleToken="Test", action="Unlock")
+doors['Door 1'].set_schedule(ScheduleToken=schedules[schedule_name].token, action="Lock", name="Door 2")
 
 print()
