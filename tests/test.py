c4d6be216b76fe727431bd02bec8c55ecef6844f
Dark Decoy <darkdecoy@notracking.space>
2025-09-12 22:48:58 +0000

    refactor: updated test script

--- tests/test.py
+++ tests/test.py
@@ -2,16 +2,18 @@ from axis_vapix.device import a1001
 import datetime
 
 schedule_name = 'Another One'
+token = ("manager01_" + "anotherone")
 name = 'Test Meeting 1'
 start = datetime.datetime(2025, 9, 18, 16, 0)
 end = datetime.datetime(2025, 9, 18, 18, 0)
 
 control1 = a1001(host="192.168.1.20", user="root", password="changem3")
 
-control1.schedule.set_schedule(name=schedule_name, token=("manager01_" + "anotherone"))
+control1.schedule.set_schedule(name=schedule_name, token=token)
 
-control1.schedule.schedules[schedule_name].add_event(name=name, start=start, end=end)
+for x in range(1):
+    control1.schedule.schedules[token].add_event(name=name, start=start, end=end)
 
-control1.doorcontrol.doors['Door 1'].set_schedule(ScheduleToken=control1.schedule.schedules[schedule_name].token, action="Unlock")
+control1.doorcontrol.doors['Door 1'].set_unlockschedules(token=list(control1.schedule.schedules[token].children), reset=False)
 
-print(control1.doorcontrol.doors['Door 1'].schedules)
+print(control1.doorcontrol.doors['Door 1'].unlockschedules)
