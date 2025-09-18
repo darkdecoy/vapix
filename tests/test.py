826b06dcb66da6e7160f3407a77ec40576f8e79d
Dark Decoy <darkdecoy@notracking.space>
2025-09-18 20:30:35 +0000

    refactor: update test.py

--- tests/test.py
+++ tests/test.py
@@ -9,11 +9,11 @@ end = datetime.datetime(2025, 9, 18, 18, 0)
 
 control1 = a1001(host="192.168.1.20", user="root", password="changem3")
 
-control1.schedule.set_schedule(name=schedule_name, token=token)
+control1.schedule.enable_schedules(token=token)
 
 for x in range(1):
     control1.schedule.schedules[token].add_event(name=name, start=start, end=end)
 
-control1.doorcontrol.doors['Door 1'].set_unlockschedules(token=list(control1.schedule.schedules[token].children), reset=False)
+control1.doorcontrol.doors['Door 1'].set_unlockschedules(token=control1.schedule.schedules[token].parent, reset=False)
 
 print(control1.doorcontrol.doors['Door 1'].unlockschedules)
