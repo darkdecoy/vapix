94f9fc89108bf42bd7172a5a18ae8f44615ad2d9
Dark Decoy <darkdecoy@notracking.space>
2025-09-25 23:35:48 +0000

    refactor: updated test script to test real world use case

--- tests/test.py
+++ tests/test.py
@@ -1,23 +1,103 @@
 from axis_vapix.device import a1001
 import datetime
 
-schedule_name = 'Another One'
-token = ("manager01_" + "anotherone")
-name = 'Test Meeting 1'
-start = datetime.datetime(2025, 7, 18, 16, 0)
-end = datetime.datetime(2025, 7, 18, 18, 0)
+def init():
 
-control1 = a1001(host="192.168.1.20", user="root", password="changem3")
+    data = {
+        "controller": {
+            "host": "192.168.1.20",
+            "user": "root",
+            "password": "changem3",
+            "doors": [{
+                "name": "Door 1",
+                "postfix_pattern": " %Y %m",
+                "tags": ["Manager01"]}]},
+        "schedule": {
+            "name": 'AnotherOne',
+            "token": ("manager01_" + "anotherone"),
+            "tokens": list()},
+        "event": {
+            "name": 'Test Meeting 1',
+            "starts_at": datetime.datetime(2025, 7, 18, 16, 0),
+            "ends_at": datetime.datetime(2025, 7, 18, 18, 0)}}
 
-control1.schedule.create_schedule(name=schedule_name, operator="addition", token=token)
+    data["schedule"]["tokens"].append(data["schedule"]["token"])
 
-for x in range(1):
-    control1.schedule.schedules[token].add_event(name=(name + str(x)), start=start, end=end)
+    data["controller"]["api"] = a1001(
+            host=data["controller"]["host"],
+            user=data["controller"]["user"],
+            password=data["controller"]["password"],
+        )
 
-    control1.schedule.schedules[token].remove_pastevents()
+    return data
 
-control1.schedule.remove_schedule(token=token)
+def poll(controller, data):
 
-control1.doorcontrol.doors['Door 1'].set_unlockschedules(token=control1.schedule.schedules[token].parent, reset=False)
+    controller["api"].schedule.create_schedule(name=data["schedule"]["name"], operator="addition", token=data["schedule"]["token"])
 
-print(control1.doorcontrol.doors['Door 1'].unlockschedules)
+    for token in controller["api"].schedule.schedules.keys():
+
+        if token.split("_")[0].lower() in data["schedule"]["tokens"]:
+
+            controller["api"].schedule.schedules[token].enabled = True
+
+            controller["api"].schedule.schedules[token].get_schedule()
+
+    for door in controller["doors"]:
+
+        controller["api"].doorcontrol.doors[door["name"]].get_unlockschedules()
+
+    data["controller"] = controller
+    return data
+
+
+def push(controller, data):
+
+    events = 1
+
+    for door in controller["doors"]:
+
+        for tag in door["tags"]:
+
+            for x in range(events):
+
+                postfix = data["event"]["starts_at"].strftime(door["postfix_pattern"])
+                schedule_name = tag + postfix
+                schedule_token = (tag + postfix).lower().replace(" ", "_")
+
+                if schedule_token not in controller["api"].schedule.schedules.keys():
+                    controller["api"].schedule.create_schedule(
+                        name=schedule_name, token=schedule_token
+                    )
+
+                controller["api"].schedule.schedules[schedule_token].add_event(
+                    name=(data["event"]["name"] + str(x)), start=data["event"]["starts_at"], end=data["event"]["ends_at"]
+                )
+
+                controller["api"].doorcontrol.doors[door["name"]].set_unlockschedules(
+                    token=schedule_token, reset=False
+                )
+
+
+def purge(controller):
+
+    controller["api"].schedule.remove_pastschedules()
+
+    print()
+
+
+def main():
+
+    data = init()
+
+    data = poll(controller=data["controller"], data=data)
+
+    purge(controller=data["controller"])
+
+    push(controller=data["controller"], data=data)
+
+    print("...Test Completed")
+
+
+if __name__ == "__main__":
+    main()
