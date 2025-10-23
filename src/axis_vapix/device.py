b49a9a2a436920f71bf1e03a6bacf33438c2776e
Dark Decoy <darkdecoy@notracking.space>
2025-10-23 20:22:39 +0000

    refactor: make default removal limit configurable in device object

--- src/axis_vapix/device.py
+++ src/axis_vapix/device.py
@@ -7,11 +7,13 @@ class a1001:
     A class that provides an interface to interact with Axis A1001
     """
 
-    def __init__(self, host, user, password, timeout=5):
+    def __init__(self, host, user, password, timeout=5, removal_limit=30):
         """
         Initializes the Axis A1001
         """
         self.api = api(host=host, user=user, password=password, timeout=timeout)
 
+        self.removal_limit = removal_limit
+
         self.doorcontrol = DoorControl(self)
-        self.schedule = ScheduleEndpoint(self)
+        self.schedule = ScheduleEndpoint(api=self.api,removal_limit=self.removal_limit)
